d5b66cba904d92154f7c4247c1084d71d68cf9f7
HamadaDere <hamadad54321@gmail.com>
2025-09-07 05:40:53 -0700

    create product my shipment list

---
+++ flutter/spl/lib/core/utils/lists/product_my_shipment_list.dart
@@ -0,0 +1,12 @@
+List<String> productsMyShipmentList = [
+  'Laptop',
+  'Smartphone',
+  'Tablet',
+  'Headphones',
+  'Smartwatch',
+  'Keyboard',
+  'Mouse',
+  'Monitor',
+  'Printer',
+  'External Hard Drive',
+];
--- flutter/spl/lib/features/my_shipment/presentation/views/widgets/custom_my_shipment_list_view.dart
+++ flutter/spl/lib/features/my_shipment/presentation/views/widgets/custom_my_shipment_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spl/core/utils/lists/product_my_shipment_list.dart';
 import 'package:spl/features/my_shipment/presentation/views/widgets/custom_my_shipment_card.dart';
 
 class CustomMyShipmentListView extends StatelessWidget {
@@ -8,23 +9,10 @@ class CustomMyShipmentListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: const BouncingScrollPhysics(),
-      itemCount: 10,
+      itemCount: productsMyShipmentList.length,
       itemBuilder: (context, index) {
-        return CustomMyShipmentCard(title: products[index]);
+        return CustomMyShipmentCard(title: productsMyShipmentList[index]);
       },
     );
   }
 }
-
-List<String> products = [
-  'Laptop',
-  'Smartphone',
-  'Tablet',
-  'Headphones',
-  'Smartwatch',
-  'Keyboard',
-  'Mouse',
-  'Monitor',
-  'Printer',
-  'External Hard Drive',
-];
